d7975c99b2861dba64550b708a2e7ec18b8f2c64
Fernando Ferrara <ferrarafer@gmail.com>
2023-06-22 11:19:22 -0300

    fix: bind back method to navigateBack method (#23)
    fix: Bind back method to navigateBack method (#22)
    
    Bind back method to navigateBack method

--- lib/src/navigation/router_service.dart
+++ lib/src/navigation/router_service.dart
@@ -74,8 +74,7 @@ class RouterService implements RouterServiceInterface {
 
   Future<bool> pop<T extends Object?>([T? result]) => router.pop(result);
 
-  /// An alias for pop for better readability
-  Future<bool> back<T extends Object?>({T? result}) => router.pop(result);
+  void back<T extends Object?>({T? result}) => router.navigateBack();
 
   RoutingController topMostRouter({bool ignorePagelessRoutes = false}) =>
       router.topMostRouter(
